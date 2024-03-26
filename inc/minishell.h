410085edbb0d861750b2eb5c9936c8553573869f
Jonas Götz <jonas.goetz01@web.de>
2024-03-26 21:37:35 +0100

    neuer tokenizer

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: jgotz <jgotz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 18:47:15 by jgotz             #+#    #+#             */
-/*   Updated: 2024/03/25 12:53:50 by jgotz            ###   ########.fr       */
+/*   Updated: 2024/03/26 14:16:35 by jgotz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,60 +32,84 @@
 typedef enum e_token_type
 {
 	TOKEN_NUMBER,
+	TOKEN_WORD,
 	TOKEN_PLUS,
 	TOKEN_MINUS,
 	TOKEN_MULT,
 	TOKEN_DIV,
 	TOKEN_BRACKET_L,
-	TOKEN_BRACKET_R
-}					t_token_type;
+	TOKEN_BRACKET_R,
+	TOKEN_DOUBLE_LESS,
+	TOKEN_LESS,
+	TOKEN_GREATER,
+	TOKEN_DOUBLE_GREATER,
+	TOKEN_DOUBLE_PIPE,
+	TOKEN_PIPE,
+	TOKEN_AMPERSAND,
+	TOKEN_DOUBLE_AMPERSAND,
+	TOKEN_DOUBLE_QUOTE,
+	TOKEN_SINGLE_QUOTE,
+	TOKEN_SEMICOLON,
+}						t_token_type;
 
 typedef struct s_token
 {
-	t_token_type	type;
-	char			*value;
-	struct s_token	*next;
-}					t_token;
+	t_token_type		type;
+	char				*value;
+	struct s_token		*next;
+}						t_token;
 
 typedef struct s_stack
 {
-	struct s_token	*array;
-	size_t			capacity;
-	size_t			size;
-}					t_stack;
+	struct s_token		*array;
+	size_t				capacity;
+	size_t				size;
+}						t_stack;
 
-int					show_prompt(char **envv);
+typedef struct s_ast_node
+{
+	t_token_type		type;
+	char				*value;
+	struct s_ast_node	*left;
+	struct s_ast_node	*right;
+}						t_ast_node;
 
-char				*get_current_dir(void);
-void				change_dir(char *path);
+int						show_prompt(char **envv);
 
-void				handle_sigint(int sig);
-void				handle_sigquit(int sig);
+char					*get_current_dir(void);
+void					change_dir(char *path);
 
-void				print_envs(char **envv);
-char				*get_env(char *env);
-char				*get_hostname(void);
-void				print_tokens(t_token *tokens);
-void				print_tokens_value(t_token *tokens);
-void				remove_unused_spaces(t_token *tokens);
-char				*execute_command(const char *command);
+void					handle_sigint(int sig);
+void					handle_sigquit(int sig);
 
-t_token				*tokenize(const char *input);
-t_stack				*toPostFix(t_token *tokens, int numTokens);
+void					print_envs(char **envv);
+char					*get_env(char *env);
+char					*get_hostname(void);
+void					print_tokens(t_token *tokens);
+void					print_tokens_value(t_token *tokens);
+void					remove_unused_spaces(t_token **tokens);
+char					*execute_command(const char *command);
 
-t_token				*postfixFromTokens(t_token *tokens);
+t_token					*tokenize(const char *input);
+t_stack					*toPostFix(t_token *tokens, int numTokens);
+t_ast_node				*parse_tokens_to_ast(t_token *tokens);
+void					print_ast_execution_order(t_ast_node *root);
 
-t_stack				*createStack(size_t capacity);
-void				push(t_stack *stack, t_token token);
-t_token				pop(t_stack *stack);
-t_token				peek(const t_stack *stack);
-void				freeStack(t_stack *stack);
+t_token					*postfixFromTokens(t_token *tokens);
+int						precedence_node(t_ast_node *node);
+int						precedence(t_token token);
 
-int					validator(char *input);
+t_stack					*createStack(size_t capacity);
+void					push(t_stack *stack, t_token token);
+t_token					pop(t_stack *stack);
+t_token					peek(const t_stack *stack);
+void					freeStack(t_stack *stack);
 
-t_token				*create_token(t_token_type type, char *value);
-void				free_tokens(t_token *tokens);
-void				append_token(t_token **head, t_token *new_token);
-size_t				token_count(t_token *tokens);
+int						validator(char *input);
+
+t_token					*create_token(t_token_type type, char *value);
+void					free_tokens(t_token *tokens);
+void					append_token(t_token **head, t_token *new_token);
+size_t					token_count(t_token *tokens);
 
 #endif
