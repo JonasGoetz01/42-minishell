d070d7b513375ff2ab8836f865b63b17485a47d4
Jonas Götz <jonas.goetz01@web.de>
2024-04-15 17:44:01 +0200

    improved tests

--- tests/unit/test.c
+++ tests/unit/test.c
@@ -1,13 +1,19 @@
-#include "../../inc/minishell.h"
 #include "test.h"
+#include <stdio.h>
+
+TEST_COUNT(1);
 
 int	main(void)
 {
-	printf("Testing...!\n");
-	TEST("Add", "Should add two numbers", {
+	INIT_TESTER();
+	TEST("add", "Should add two numbers", {
 		ASSERT_EQ(2 + 2, 4);
 		ASSERT_EQ(2 + 3, 4);
 	});
-
+	TEST("sub", "Should substract two numbers", {
+		ASSERT_EQ(2 - 2, 0);
+		ASSERT_EQ(2 - 3, -1);
+	});
+	SUMMARIZE_TESTS();
 	return (0);
 }
--- tests/unit/test.h
+++ tests/unit/test.h
@@ -1,25 +1,52 @@
 #ifndef TEST_H
 # define TEST_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define macros to keep track of test counts
+#define TEST_COUNT(count) \
+    static int totalTests = count; \
+    static int passedTests = 0; \
+    static int failedTests = 0;
+
+#define GET_TOTAL_TESTS() totalTests
+#define GET_PASSED_TESTS() passedTests
+#define GET_FAILED_TESTS() failedTests
+
+// Macro to initialize the tester and print a welcome message
+#define INIT_TESTER() \
+    printf("\x1b[34mStarting tests...\n\x1b[0m");
+
+// Macro to summarize the tests
+#define SUMMARIZE_TESTS() \
+    totalTests = passedTests + failedTests; \
+    if(failedTests == 0) \
+    { \
+      printf("\x1b[32m[  PASSED  ] %d of %d passed\n\x1b[0m", passedTests, totalTests); \
+    } \
+    else \
+    { \
+      printf("\x1b[31m[  FAILED  ] %d of %d failed\n\x1b[0m", failedTests, totalTests); \
+    } \
+
 #define TEST(function, description, body) \
-      do { \
-           printf("%s\n", function);         \
-           printf("%s\n", description);   \
-           (body);                     \
-      } while (0)
+    do { \
+        printf("\x1b[32m[----------] %s.", function); \
+        printf("%s\x1b[0m\n", description); \
+        (body); \
+        printf("\n");                      \
+    } while (0)
 
 #define ASSERT_EQ(arg1, arg2) \
-      do { \
-           if (arg1 == arg2) \
-           { \
-            printf("OK\n"); \
-           } \
-           else \
-           { \
-            printf("KO\n"); \
-            exit(1); \
-           } \
-      } while (0)
-
+    do { \
+        if (arg1 == arg2) { \
+            printf("\x1b[32m[    OK    ] %d == %d\x1b[0m\n", arg1, arg2); \
+            passedTests++;  \
+        } else { \
+            printf("\x1b[31m[    KO    ] %d == %d\x1b[0m\n", arg1, arg2); \
+            failedTests++; \
+        } \
+    } while (0)
 
 #endif
