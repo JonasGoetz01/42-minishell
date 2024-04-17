e752691ac6ab3edbd94eebfdb4f9b7dd46462b9c
pgrossma <pgrossma@student.42heilbronn.de>
2024-04-17 17:21:35 +0200

    changed error output to stderr

--- src/buildins/error_buildins.c
+++ src/buildins/error_buildins.c
@@ -3,8 +3,14 @@
 void	ft_error_buildin(const char *msg, const char *arg, t_process *process, int exit_status)
 {
 	process->exit_status = exit_status;
+	ft_putstr_fd("minishell: ", 2);
+	ft_putstr_fd(process->cmd, 2);
+	ft_putstr_fd(": ", 2);
 	if (arg)
-		printf("minishell: %s: %s: %s\n", process->cmd, arg, msg);
-	else
-		printf("minishell: %s: %s\n", process->cmd, msg);
+	{
+		ft_putstr_fd(arg, 2);
+		ft_putstr_fd(": ", 2);
+	}
+	ft_putstr_fd(msg, 2);
+	ft_putstr_fd("\n", 2);
 }
