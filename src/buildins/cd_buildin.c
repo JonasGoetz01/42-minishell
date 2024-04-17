341eb2ba6ed44bda1c56f90af01a5270a2350ed0
pgrossma <pgrossma@student.42heilbronn.de>
2024-04-17 19:03:42 +0200

    fixed set_env func and added cd -

--- src/buildins/cd_buildin.c
+++ src/buildins/cd_buildin.c
@@ -1,6 +1,6 @@
 #include "../../inc/minishell.h"
 
-void	ft_cd_dir_rel(t_process *process)
+static void	ft_cd_dir_rel(t_process *process)
 {
 	char	*cwd;
 	char	*new_path;
@@ -25,26 +25,56 @@ void	ft_cd_dir_rel(t_process *process)
 	return ;
 }
 
-void	ft_cd_buildin(t_process *process)
+static char	*ft_cd_env(const char *env, t_process *process)
+{
+	char	*new_pwd;
+	char	*err_msg;
+
+	new_pwd = getenv(env);
+	if (!new_pwd)
+	{
+		err_msg = ft_strjoin(env, " not set");
+		ft_error_buildin(err_msg, NULL, process, 1);
+		free(err_msg);
+	}
+	return (new_pwd);
+}
+
+static void	ft_set_oldpwd(t_global *global)
+{
+	char	*old_pwd;
+	char	*pwd;
+
+	pwd = getcwd(NULL, 0);
+	old_pwd = ft_strjoin("OLDPWD=", pwd);
+	free(pwd);
+	ft_set_env(old_pwd, global);
+	free(old_pwd);
+}
+
+void	ft_cd_buildin(t_process *process, t_global *global)
 {
 	char	*tmp;
 
 	if (!process->args[1])
+		tmp = ft_cd_env("HOME", process);
+	else if (ft_strncmp(process->args[1], "-", 2) == 0)
 	{
-		tmp = getenv("HOME");
-		if (!tmp)
-		{
-			ft_error_buildin("HOME not set", NULL, process, 1);
-			return ;
-		}
+		tmp = ft_cd_env("OLDPWD", process);
+		if (tmp)
+			printf("%s\n", tmp);
 	}
 	else if (process->args[1][0] == '/' || process->args[1][0] == '.')
 		tmp = process->args[1];
 	else
 	{
+		ft_set_oldpwd(global);
 		ft_cd_dir_rel(process);
 		return ;
 	}
+	ft_set_oldpwd(global);
+	if (!tmp)
+		return ;
 	if (chdir(tmp) != 0)
 		ft_error_buildin(strerror(errno), process->args[1], process, 1);
 }
