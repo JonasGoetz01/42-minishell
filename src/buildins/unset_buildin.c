e4ddfe8ce3ece11726325b81de11cbbbd1973944
pgrossma <pgrossma@student.42heilbronn.de>
2024-04-20 19:12:04 +0200

    unset now accepts multi args

--- src/buildins/unset_buildin.c
+++ src/buildins/unset_buildin.c
@@ -2,7 +2,14 @@
 
 void	ft_unset_buildin(t_process *process, t_global *global)
 {
+	size_t	ind;
+
 	if(!process->args[1])
 		return ;
-	ft_unset_env(process->args[1], global);
+	ind = 1;
+	while (process->args[ind])
+	{
+		ft_unset_env(process->args[ind], global);
+		ind++;
+	}
 }
