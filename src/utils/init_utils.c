874dc7a23742ddf2fab6f4bb8701190c3d07d589
pgrossma <pgrossma@student.42heilbronn.de>
2024-04-20 19:00:20 +0200

    added right parenthesis for export buildin

--- src/utils/init_utils.c
+++ src/utils/init_utils.c
@@ -1,13 +1,41 @@
 #include "../../inc/minishell.h"
 
+static bool	ft_env_cpy(char **src, char ***dst)
+{
+	char	*name;
+	char	*value;
+	bool	result;
+
+	while (*src)
+	{
+		name = ft_trim_to_equal(*src);
+		value = ft_trim_from_equal(*src);
+		if (!name || !value)
+		{
+			if (name)
+				free(name);
+			if (value)
+				free(value);
+			return (false);
+		}
+		result = ft_add_env_export(name, value, dst);
+		free(name);
+		free(value);
+		if (!result)
+			return (false);
+		src++;
+	}
+	return (true);
+}
+
 void	ft_init_t_global(t_global *global, char **envv)
 {
 	global->envv = ft_arr_create_len(ft_arr_len(envv) + 1);
 	if (!global->envv)
 		exit(1);
 	ft_arr_cpy(envv, global->envv);
-	global->env_export = ft_arr_create_len(ft_arr_len(envv) + 1);
+	global->env_export = ft_arr_create();
 	if (!global->env_export)
 		exit(1);
-	ft_arr_cpy(envv, global->env_export);
+	ft_env_cpy(global->envv, &global->env_export);
 }
