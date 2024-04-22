2a8d6db059c6197db47a169ca08d6a29c1406614
Jonas Götz <jonas.goetz01@web.de>
2024-04-22 14:24:02 +0200

    expand variables inside of bigger string

--- src/input_parsing/expander.c
+++ src/input_parsing/expander.c
@@ -26,6 +26,9 @@ void	ft_expand_tokens(t_token *tokens, t_global *global)
 {
 	t_token	*current_token;
 	int		in_single_quotes;
+	int		start;
+	int		end;
+	char	*temp;
 
 	current_token = tokens;
 	in_single_quotes = 0;
@@ -34,7 +37,27 @@ void	ft_expand_tokens(t_token *tokens, t_global *global)
 		if (current_token->type == TOKEN_SINGLE_QUOTE)
 			in_single_quotes = !in_single_quotes;
 		if (current_token->type == TOKEN_WORD && !in_single_quotes)
-			current_token->value = ft_expand_word(current_token->value, global);
+		{
+			start = 0;
+			while (current_token->value[start] != '$'
+				&& current_token->value[start] != '\0')
+				start++;
+			end = start;
+			while (current_token->value[end] != '\0'
+				&& current_token->value[end] != ' '
+				&& current_token->value[end] != '?')
+				end++;
+			if (current_token->value[end] == ' ')
+				end--;
+			temp = ft_strdup("");
+			temp = ft_strjoin(temp, ft_substr(current_token->value, 0, start));
+			temp = ft_strjoin(temp,
+					ft_expand_word(ft_substr(current_token->value, start, end
+							- start + 1), global));
+			temp = ft_strjoin(temp, ft_strdup(&current_token->value[end + 1]));
+			free(current_token->value);
+			current_token->value = temp;
+		}
 		current_token = current_token->next;
 	}
 }
