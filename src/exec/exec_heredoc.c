092ba38792166f2a2d93f02184b8801cc9286080
pgrossma <pgrossma@student.42heilbronn.de>
2024-04-23 18:57:04 +0000

    changed here doc to get_next_line

--- src/exec/exec_heredoc.c
+++ src/exec/exec_heredoc.c
@@ -8,18 +8,17 @@ void	ft_exec_here_doc(t_ast_node *node, t_global *global)
 
 	limiter = node->right->token->value;
 	if (pipe(fd_pipe) == -1)
-	{
-		ft_print_error(strerror(errno), NULL);
-		return ;
-	}
+		return (ft_print_error(strerror(errno), NULL));
 	while (true)
 	{
-		line = readline("> ");
+		ft_putstr_fd("> ", STDOUT_FILENO);
+		line = get_next_line(STDIN_FILENO);
 		if (!line)
 			break ;
 		if (ft_strncmp(line, limiter, ft_strlen(limiter) + 1) == 0)
 		{
 			free(line);
+			get_next_line(-1);
 			break ;
 		}
 		ft_putstr_fd(line, fd_pipe[PIPE_WRITE]);
