c2ef5209b3088f7ca001c926b982cc976a19bce1
pgrossma <pgrossma@student.42heilbronn.de>
2024-04-24 14:16:38 +0000

    added more checks to exit buildin

--- src/buildins/exit_buildin.c
+++ src/buildins/exit_buildin.c
@@ -6,13 +6,10 @@ void	ft_exit_buildin(t_process *process)
 	char	*arg1;
 
 	arg1 = process->args[1];
+	if (isatty(fileno(stdin)))
+		ft_putstr_fd("exit\n", 1);
 	if (!arg1)
-		exit(1);
-	if (process->args[2])
-	{
-		ft_error_buildin("too many arguments", NULL, process, 1);
-		return ;
-	}
+		exit(0);
 	ind = 0;
 	if (arg1[ind] == '+' || arg1[ind] == '-')
 		ind++;
@@ -20,10 +17,20 @@ void	ft_exit_buildin(t_process *process)
 	{
 		if (!ft_isdigit(arg1[ind]))
 		{
-			ft_error_buildin("numeric argument required", arg1, process, 1);
-			exit(1);
+			ft_error_buildin("numeric argument required", arg1, process, 2);
+			exit(2);
 		}
 		ind++;
+	}
+	if (process->args[2])
+	{
+		ft_error_buildin("too many arguments", NULL, process, 1);
+		return ;
+	}
+	if (arg1[0] == 0)
+	{
+		ft_error_buildin("numeric argument required", arg1, process, 2);
+		exit(2);
 	}
 	exit(ft_atoi(arg1));
 }
