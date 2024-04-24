a788bf79fa4bee635a25fd0f6c9c67665bf2561e
pgrossma <pgrossma@student.42heilbronn.de>
2024-04-24 14:24:23 +0000

    echo now handels multiple -n correctly

--- src/buildins/echo_buildin.c
+++ src/buildins/echo_buildin.c
@@ -5,13 +5,18 @@ void	ft_echo_buildin(t_process *process)
 	bool	new_line;
 	size_t	ind;
 
-	if (process->args[1])
-		new_line = (ft_strncmp(process->args[1], "-n", 3) != 0);
-	else
-		new_line = true;
 	ind = 1;
-	if (!new_line)
+	new_line = true;
+	while (true)
+	{
+		if (process->args[ind] == NULL)
+			break ;
+		if (ft_strncmp(process->args[ind], "-n", 3) == 0)
+			new_line = false;
+		else
+			break ;
 		ind++;
+	}
 	while (process->args[ind])
 	{
 		printf("%s", process->args[ind]);
