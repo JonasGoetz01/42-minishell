cef7107b522e15da0f02d928d2d03e4e13ad51b8
Jonas Götz <jonas.goetz@egotec.com>
2024-05-02 21:03:56 +0000

    check if rearrange even does something

--- src/input_parsing/rearrange_tokens.c
+++ src/input_parsing/rearrange_tokens.c
@@ -33,57 +33,63 @@ void	rearrange_tokens(t_token **tokens)
 		current = current->next;
 	}
 	// If < is the first token
-	if (prev == NULL && current != NULL && current->next
-		&& current->next->type == TOKEN_WORD && current->next->next
-		&& current->next->next->type == TOKEN_WORD)
+	// if (prev == NULL && current != NULL && current->next
+	// 	&& current->next->type == TOKEN_WORD && current->next->next
+	// 	&& current->next->next->type == TOKEN_WORD)
+	// {
+	// 	redirect = *tokens;
+	// 	file = redirect->next;
+	// 	after_file = file->next;
+	// 	current = after_file;
+	// 	*tokens = after_file;
+	// 	while (current != NULL && current->type == TOKEN_WORD)
+	// 		current = current->next;
+	// 	while (after_file != NULL && after_file->next != NULL
+	// 		&& after_file->next->type == TOKEN_WORD)
+	// 		after_file = after_file->next;
+	// 	file->next = after_file->next;
+	// 	after_file->next = redirect;
+	// 	if (file->next != NULL)
+	// 		rearrange_tokens(&file->next);
+	// }
+	// else if (prev == NULL && current != NULL && current->next
+	// 	&& current->next->type == TOKEN_WORD && !current->next->next)
+	// {
+	// 	redirect = *tokens;
+	// 	file = redirect->next;
+	// 	current = create_token(TOKEN_SPACE, ft_strdup(" "));
+	// 	current->next = redirect;
+	// 	*tokens = current;
+	// }
+	// // If < is not the first token
+	// else if (current != NULL && (prev == NULL || (prev->type != TOKEN_WORD
+	// 			&& prev->type != TOKEN_DOUBLE_QUOTE
+	// 			&& prev->type != TOKEN_SINGLE_QUOTE)) && current->next
+	// 	&& current->next->type == TOKEN_WORD && current->next->next
+	// 	&& current->next->next->type == TOKEN_WORD)
+	// {
+	// 	redirect = current;
+	// 	file = current->next;
+	// 	after_file = file->next;
+	// 	current = after_file;
+	// 	while (current != NULL && current->type == TOKEN_WORD)
+	// 		current = current->next;
+	// 	if (prev != NULL)
+	// 		prev->next = after_file;
+	// 	else
+	// 		(*tokens)->next = after_file;
+	// 	while (after_file != NULL && after_file->next != NULL
+	// 		&& after_file->next->type == TOKEN_WORD)
+	// 		after_file = after_file->next;
+	// 	file->next = after_file->next;
+	// 	after_file->next = redirect;
+	// 	if (file->next != NULL)
+	// 		rearrange_tokens(&file->next);
+	// }
+	// > needs to be rearranged
+	prev_link_list(tokens);
+	if (current && (current->type == TOKEN_GREATER || current->type == TOKEN_DOUBLE_GREATER))
 	{
-		redirect = *tokens;
-		file = redirect->next;
-		after_file = file->next;
-		current = after_file;
-		*tokens = after_file;
-		while (current != NULL && current->type == TOKEN_WORD)
-			current = current->next;
-		while (after_file != NULL && after_file->next != NULL
-			&& after_file->next->type == TOKEN_WORD)
-			after_file = after_file->next;
-		file->next = after_file->next;
-		after_file->next = redirect;
-		if (file->next != NULL)
-			rearrange_tokens(&file->next);
-	}
-	else if (prev == NULL && current != NULL && current->next
-		&& current->next->type == TOKEN_WORD && !current->next->next)
-	{
-		redirect = *tokens;
-		file = redirect->next;
-		current = create_token(TOKEN_SPACE, ft_strdup(" "));
-		current->next = redirect;
-		*tokens = current;
-	}
-	// If < is not the first token
-	else if (current != NULL && (prev == NULL || (prev->type != TOKEN_WORD
-				&& prev->type != TOKEN_DOUBLE_QUOTE
-				&& prev->type != TOKEN_SINGLE_QUOTE)) && current->next
-		&& current->next->type == TOKEN_WORD && current->next->next
-		&& current->next->next->type == TOKEN_WORD)
-	{
-		redirect = current;
-		file = current->next;
-		after_file = file->next;
-		current = after_file;
-		while (current != NULL && current->type == TOKEN_WORD)
-			current = current->next;
-		if (prev != NULL)
-			prev->next = after_file;
-		else
-			(*tokens)->next = after_file;
-		while (after_file != NULL && after_file->next != NULL
-			&& after_file->next->type == TOKEN_WORD)
-			after_file = after_file->next;
-		file->next = after_file->next;
-		after_file->next = redirect;
-		if (file->next != NULL)
-			rearrange_tokens(&file->next);
+
 	}
 }
