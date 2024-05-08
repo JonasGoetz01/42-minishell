2a721be906d5f0e61361ce301078823dcbb76e16
pgrossma <pgrossma@student.42heilbronn.de>
2024-05-08 11:23:34 +0000

    main norm

--- src/main.c
+++ src/main.c
@@ -8,31 +8,21 @@ void	print_welcome_message(t_global *global)
 
 	pgrossma = "https://profile.intra.42.fr/users/pgrossma";
 	jgotz = "https://profile.intra.42.fr/users/jgotz";
-	username = ft_get_env("USER", global->envv);
-	char *pastel_colors[] = {
-		"\x1b[38;2;255;204;204m",
-		"\x1b[38;2;255;229;204m",
-		"\x1b[38;2;255;242;204m",
-		"\x1b[38;2;204;255;204m",
-		"\x1b[38;2;204;255;255m",
-		"\x1b[38;2;204;204;255m",
-		"\x1b[38;2;255;204;255m",
-	};
-	printf("\x1b[2J%s", pastel_colors[0]);
-	printf("%s\t██████  ██████  ███████ ██   ██\n", pastel_colors[1]);
-	printf("%s\t██   ██ ██   ██ ██      ██   ██\n", pastel_colors[2]);
-	printf("%s\t██   ██ ██████  ███████ ███████\n", pastel_colors[3]);
-	printf("%s\t██   ██ ██           ██ ██   ██\n", pastel_colors[4]);
-	printf("%s\t██████  ██      ███████ ██   ██\n", pastel_colors[5]);
+	printf("\x1b[2J%s", PAS1);
+	printf("%s\t██████  ██████  ███████ ██   ██\n", PAS2);
+	printf("%s\t██   ██ ██   ██ ██      ██   ██\n", PAS3);
+	printf("%s\t██   ██ ██████  ███████ ███████\n", PAS4);
+	printf("%s\t██   ██ ██           ██ ██   ██\n", PAS5);
+	printf("%s\t██████  ██      ███████ ██   ██\n", PAS6);
 	printf("\x1b[0m\n\n\t\tDont panic shell\n\n");
 	printf("\t\t\e]8;;%s\apgrossma\e]8;;\a && \e]8;;%s\ajgotz\e]8;;\a\n\n\n",
 		pgrossma, jgotz);
+	username = ft_get_env("USER", global->envv);
 	if (username)
 	{
 		printf("\n✨ Hello, %s ✨\n", username);
 		free(username);
 	}
-	fflush(stdout);
 }
 
 int	main(int argc, char **argv, char **envv)
